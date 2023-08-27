f10f97fdf347aced106901e65a934a99006a6061
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2023-08-27 11:06:39 +0300

    Adds more examples of logs

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,13 @@
 #include <spdlog/spdlog.h>
 
 int main() {
+
+	spdlog::set_level(spdlog::level::debug);
+
+	spdlog::debug("opening the program");
 	spdlog::info("opening the program");
+	spdlog::warn("opening the program");
+	spdlog::error("opening the program");
 
 
 	int a;
